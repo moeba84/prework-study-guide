916f0e1661b1c30baf9cd5e15cbe1a4bbfc759bc
moeba84 <119361569+moeba84@users.noreply.github.com>
2022-12-12 22:14:48 -0800

    <add js>

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,21 +1,24 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x=0; x <topics.length; x++){
+var randomTopic= topics[Math.floor( Math.random()*topics.length)];
+function listTopics(){
+for(var x=0; x <topics.length; x++){
     console.log (topics[x]);
 }
-/*
+}
+function selectTopics (){
 
-if (topic==='HTML'){
+if (randomTopic==='HTML'){
     console.log("Let's Study HTML!");
-} else if (topic==='CSS'){
+} else if (randomTopic==='CSS'){
     console.log( "Let's Study Code!");
-} else if (topic==='Git'){
+} else if (randomTopic==='Git'){
     console.log ("Let's Study Git!");
-} else if (topic==='JavaScript'){
+} else if (randomTopic==='JavaScript'){
 console.log( "let's Study JavaScript!");
 } else{console.log ('Please Try Again');
 }
-*/
-
-
-
-
+}
+console.log('Here are the topics we learned through prework:');
+listTopics ()
+console.log('Which topic should we study first?');
+selectTopics ()
